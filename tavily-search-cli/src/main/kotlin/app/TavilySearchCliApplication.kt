97fb6581785d755efa1ee4ccb1ed37dfdf46c288
Stefan Baramov <sbaramov@gmail.com>
2026-04-04 14:20:40 -0400

    fix configuration but still does not open a command prompt

--- tavily-search-cli/src/main/kotlin/app/TavilySearchCliApplication.kt
+++ tavily-search-cli/src/main/kotlin/app/TavilySearchCliApplication.kt
@@ -2,8 +2,10 @@ package app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.shell.command.annotation.EnableCommand
 
 @SpringBootApplication
+@EnableCommand
 class TavilySearchCliApplication
 
 fun main(args: Array<String>) {
